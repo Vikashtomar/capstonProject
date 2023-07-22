d016cc1d2e81f468986552926b147fd11b22d1cf
vikash <vikash.tomar.india@gmail.com>
2023-07-22 18:12:28 +0530

    added files

---
+++ src/Components/About/About.js
@@ -0,0 +1,25 @@
+
+
+
+import React from 'react'
+import './about.css'
+
+function About() {
+  return (
+    <div className="about-page">
+        <h1>About Our Bookshelf Website</h1>
+           <p>
+             Welcome to our bookshelf website! We are a team of book enthusiasts who
+             love to explore the world of literature. Our goal is to create a
+            platform where book lovers can discover new books, organize their
+            reading lists, and connect with fellow readers.
+          </p>
+          <h2>Meet the Team</h2>
+         
+        </div>
+  
+  )
+}
+
+
+export default About
