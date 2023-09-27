e66cb970ed20a23dc926445a6e1f84687d86ac54
vikash <vikash.tomar.india@gmail.com>
2023-09-27 21:19:30 +0530

    update

--- src/Components/BookshelfFolder/Bookshelf.js
+++ src/Components/BookshelfFolder/Bookshelf.js
@@ -12,8 +12,9 @@ function Bookshelf() {
   <h1>
     
 "Welcome to our captivating bookshelf, where imagination knows no bounds. Discover a world of diverse stories that will inspire, entertain, and leave you spellbound."
+
   </h1>
-  <div className="imgbox">
+  <div className="imgbox">  
     <img src={mainimg} alt="" />
   </div>
    </div>
